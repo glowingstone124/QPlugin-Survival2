33f52583d6fff65c83f18f1ca7d5627311c59b5e
Dragon740f <csbios@163.com>
2023-10-06 14:19:10 +0800

    ChatSync

---
+++ src/main/java/vip/qoriginal/quantumplugin/ChatSync.java
@@ -0,0 +1,29 @@
+package vip.qoriginal.quantumplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerChatEvent;
+import vip.qoriginal.quantumplugin.Request;
+
+import java.util.TimerTask;
+
+public class ChatSync extends TimerTask implements Listener{
+    @EventHandler
+    public void onPlayerChatEvent(AsyncPlayerChatEvent event) throws Exception{
+        StringBuilder sb = new StringBuilder();
+        sb.append("<Survival:").append(event.getPlayer().getName()).append(">").append(event.getMessage());
+        Request.sendPostRequest("http://localhost:8080/qo/survival/msgupload", sb.toString());
+    }
+
+    @Override
+    public void run() {
+        try {
+            String result = Request.sendGetRequest("http://localhost:8080/qo/creative/msgdownload");
+            Bukkit.broadcastMessage(result);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
