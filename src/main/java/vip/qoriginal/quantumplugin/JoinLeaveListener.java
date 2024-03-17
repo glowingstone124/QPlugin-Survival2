1eb6e0675e60038202508e46d52be8b3378efeb8
glowingstone124 <hanserofficial@outlook.com>
2024-03-17 12:59:05 +0800

    new msg

--- src/main/java/vip/qoriginal/quantumplugin/JoinLeaveListener.java
+++ src/main/java/vip/qoriginal/quantumplugin/JoinLeaveListener.java
@@ -28,9 +28,9 @@ public class JoinLeaveListener implements Listener {
             event.getPlayer().sendMessage(Component.text("请稍等，我们需要对您的身份进行验证"));
             BindResponse relationship = new Gson().fromJson(Request.sendGetRequest("http://127.0.0.1:8080/qo/download/registry?name=" + event.getPlayer().getName()), BindResponse.class);
             if (relationship.code == 1) {
-                event.getPlayer().kick(Component.text("验证失败，请在群：946085440中下载QCommunity").append(Component.text("并绑定你的游戏名：" + event.getPlayer().getName()).decorate(TextDecoration.BOLD)).append(Component.text(" 之后重试！")));
+                event.getPlayer().kick(Component.text("[401 Unauthorized]验证失败，请在群：946085440中下载QCommunity").append(Component.text("并绑定你的游戏名：" + event.getPlayer().getName()).decorate(TextDecoration.BOLD)).append(Component.text(" 之后重试！")));
             } else if (relationship.frozen) {
-                event.getPlayer().kick(Component.text("验证失败，原因：您的账户已经被冻结！ ").append(Component.text("您的游戏名：" + event.getPlayer().getName()).decorate(TextDecoration.BOLD)).append(Component.text(" 请私聊群主：1294915648了解更多")));
+                event.getPlayer().kick(Component.text("[403 Forbidden]验证失败，原因：您的账户已经被冻结！ ").append(Component.text("您的游戏名：" + event.getPlayer().getName()).decorate(TextDecoration.BOLD)).append(Component.text(" 请私聊群主：1294915648了解更多")));
             } else {
                 cs.sendChatMsg("玩家" + event.getPlayer().getName() + "加入了服务器。");
                 event.getPlayer().sendMessage(Component.text("验证通过，欢迎回到Quantum Original！").appendNewline().append(Component.text("QQ: " + relationship.qq).color(TextColor.color(114, 114, 114))));
