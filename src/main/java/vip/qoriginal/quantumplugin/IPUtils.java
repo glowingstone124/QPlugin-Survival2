89166b08b04cb6c2eb6fbc208745b9d9f2004ce0
glowingstone124 <hanserofficial@outlook.com>
2024-06-08 14:37:25 +0800

    use shakaianee's ip api

--- src/main/java/vip/qoriginal/quantumplugin/IPUtils.java
+++ src/main/java/vip/qoriginal/quantumplugin/IPUtils.java
@@ -6,13 +6,15 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 import org.json.JSONObject;
 
+import java.util.Objects;
+
 public class IPUtils{
     public static void locIsCn(PlayerJoinEvent event, Plugin plugin) throws Exception {
         Player player = event.getPlayer();
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
             try {
                 ChatSync cs = new ChatSync();
-                String ip = player.getAddress().getAddress().getHostAddress();
+                String ip = Objects.requireNonNull(player.getAddress()).getAddress().getHostAddress();
                 JSONObject ipLocObj = new JSONObject(Request.sendGetRequest("https://ip.shakaianee.top/" + ip + "?f=json"));
                 if (!ipLocObj.getString("country_code").equals("CN")) {
                     player.sendMessage("你正在使用一个非中国大陆ip登录。");
