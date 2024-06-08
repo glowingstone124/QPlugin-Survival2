bb9859494615dc5484af1cb06a5d2b91c6740bfc
glowingstone124 <hanserofficial@outlook.com>
2024-06-08 16:25:37 +0800

    parse CQ

--- src/main/java/vip/qoriginal/quantumplugin/WebMsgGetter.java
+++ src/main/java/vip/qoriginal/quantumplugin/WebMsgGetter.java
@@ -9,33 +9,36 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebMsgGetter extends TimerTask{
     static ArrayList<String> buffer = new ArrayList<>();
     @Override
     public void run(){
-            try {
-                String response = Request.sendGetRequest("http://qoriginal.vip:8080/qo/msglist/download");
-                JSONObject remoteObj = new JSONObject(response);
-                JSONArray remoteRaw = remoteObj.getJSONArray("messages");
-                ArrayList<String> remoteArr = parseArrList(remoteRaw);
-                ArrayList<String> newMessages = new ArrayList<>();
-                if (buffer == null || diff(remoteArr, buffer)) {
-                    newMessages = new ArrayList<>(remoteArr);
-                    if (!newMessages.isEmpty()) {
-                        newMessages.removeAll(buffer);
-                    }
+        try {
+            String response = Request.sendGetRequest("http://qoriginal.vip:8080/qo/msglist/download");
+            JSONObject remoteObj = new JSONObject(response);
+            JSONArray remoteRaw = remoteObj.getJSONArray("messages");
+            ArrayList<String> remoteArr = parseArrList(remoteRaw);
+            ArrayList<String> newMessages = new ArrayList<>();
+            if (buffer == null || diff(remoteArr, buffer)) {
+                newMessages = new ArrayList<>(remoteArr);
+                if (!newMessages.isEmpty()) {
+                    newMessages.removeAll(buffer);
                 }
-                for (String msg : newMessages) {
-                    if (msg.startsWith("[QQ]")) {
-                        Component msgComponent = Component.text(msg).color(TextColor.color(113, 159, 165));
-                        for(Player player : Bukkit.getOnlinePlayers()) player.sendMessage(msgComponent);
-                    }
-                }
-                buffer = remoteArr;
-            } catch (Exception e) {
-                throw new RuntimeException(e);
             }
+            for (String msg : newMessages) {
+                msg = processMessage(msg);
+                if (msg.startsWith("[QQ]")) {
+                    Component msgComponent = Component.text(msg).color(TextColor.color(113, 159, 165));
+                    for(Player player : Bukkit.getOnlinePlayers()) player.sendMessage(msgComponent);
+                }
+            }
+            buffer = remoteArr;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     static ArrayList<String> parseArrList(JSONArray arr) {
@@ -56,5 +59,18 @@ public class WebMsgGetter extends TimerTask{
             }
         }
         return false;
+    }
+
+    static String processMessage(String msg) {
+        // 替换 [CQ:file]
+        Pattern filePattern = Pattern.compile("\\[CQ:file,[^\\]]*\\]");
+        Matcher fileMatcher = filePattern.matcher(msg);
+        msg = fileMatcher.replaceAll("[图片]");
+        // 替换 [CQ:at]
+        Pattern atPattern = Pattern.compile("\\[CQ:at,qq=(\\d+)\\]");
+        Matcher atMatcher = atPattern.matcher(msg);
+        msg = atMatcher.replaceAll("@$1");
+
+        return msg;
     }
 }
