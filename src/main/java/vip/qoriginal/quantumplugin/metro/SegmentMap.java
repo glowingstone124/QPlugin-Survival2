d409c8c08d4c9a88e15a22b0f8065220dbcc6e0f
Baka Cirno 999 <14835957@qq.com>
2024-06-09 22:06:07 +0800

    Ver 1.14.5.5 fix ver 1 Resolved bugs.

--- src/main/java/vip/qoriginal/quantumplugin/metro/SegmentMap.java
+++ src/main/java/vip/qoriginal/quantumplugin/metro/SegmentMap.java
@@ -1,5 +1,8 @@
 package vip.qoriginal.quantumplugin.metro;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -9,6 +12,8 @@ import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SegmentMap {
     public static HashMap<String, Segment> segMap = new HashMap<>();
@@ -16,11 +21,34 @@ public class SegmentMap {
     public static World ov = Bukkit.getWorld("world");
     public static void init() {
         lineMap.put(0, new Line("C1 Monorail","#c73532"));
+        lineMap.put(3, new Line("QO Line 3","#fcd600"));
 
         segMap.put("0001",
                 new Segment(0, false, "C1上/下行区段0001", new Location[]{
                         new Location(ov, -8, 69, 304),
                         new Location(ov, 343, 63, 187)
+                }));
+        segMap.put("0002", new Segment(3, true, "出生点", new Location[]{}));
+        segMap.put("0003", new Segment(3, true, "出生点", new Location[]{}));
+        segMap.put("0004", new Segment(3, true, "清水河", new Location[]{}));
+        segMap.put("0005", new Segment(3, true, "清水河", new Location[]{}));
+        segMap.put("0006", new Segment(3, true, "高铁出生点站", new Location[]{}));
+        segMap.put("0007", new Segment(3, true, "高铁出生点站", new Location[]{}));
+        segMap.put("0008",
+                new Segment(0, false, "L3上行至清水河区段0008", new Location[]{
+                        new Location(ov, -43, 84, 534),
+                }));
+        segMap.put("0009",
+                new Segment(0, false, "L3上行至清水河区段0009", new Location[]{
+                        new Location(ov, -8, 83, 430),
+                }));
+        segMap.put("000a",
+                new Segment(0, false, "L3上行至出生点区段000a", new Location[]{
+                        new Location(ov, -8, 84, 303),
+                }));
+        segMap.put("000b",
+                new Segment(0, false, "L3上行至出生点区段000b", new Location[]{
+                        new Location(ov, -8, 65, 146),
                 }));
     }
 
@@ -33,7 +61,8 @@ public class SegmentMap {
 
     public static void refresh() {
         for(Minecart minecart: ov.getEntitiesByClass(Minecart.class)) {
-            for(String tag: minecart.getScoreboardTags()) {
+            Set<String> tags = minecart.getScoreboardTags();
+            for(String tag: tags) {
                 if(tag.indexOf("queueing-")==0 || tag.indexOf("occupied-")==0) minecart.removeScoreboardTag(tag);
             }
         }
@@ -43,7 +72,7 @@ public class SegmentMap {
         });
         segMap.forEach((key, value) -> {
             if(value.occupied == null) {
-                for(Location location : value.signal) {
+                 for(Location location : value.signal) {
                     if(location.getBlock().getType() == Material.GREEN_CONCRETE || location.getBlock().getType() == Material.RED_CONCRETE) {
                         location.getBlock().setType(Material.GREEN_CONCRETE);
                     }
@@ -54,10 +83,11 @@ public class SegmentMap {
                         location.getBlock().setType(Material.RED_CONCRETE);
                     }
                 }
+                if(value.occupied.isDead()) {value.leave(value.occupied);}
             }
 
             for(Minecart minecart: value.queueing) {
-                minecart.setVelocity(minecart.getVelocity().multiply(0.94));
+                minecart.setVelocity(minecart.getVelocity().multiply(0.88));
                 minecart.addScoreboardTag("queueing-"+key);
             }
         });
@@ -88,11 +118,30 @@ public class SegmentMap {
         }
 
         public boolean enter(Minecart minecart) {
+            if(minecart == occupied || queueing.contains(minecart)) return false;
+            if(this.station && !minecart.getPassengers().isEmpty()) minecart.getPassengers().getFirst().sendMessage(Component.text(this.dummy).color(
+                            TextColor.fromCSSHexString(lineMap.get(this.lid).color))
+                    .append(Component.text("到了。").color(TextColor.fromCSSHexString("#ffffff"))));
             if(occupied == null || occupied.isDead()) {
+                if(!minecart.getPassengers().isEmpty() && !this.station) minecart.getPassengers().getFirst().sendMessage(
+                        Component.text("您已").append(
+                                Component.text("绿灯").color(TextColor.color(0,255,0)))
+                                .append(Component.text("进入"))
+                                .append(Component.text(this.dummy).color(
+                                        TextColor.fromCSSHexString(lineMap.get(this.lid).color)))
+                                .append(Component.text("，祝您一路顺风。")));
                 occupied = minecart;
                 return true;
             } else {
-                queueing.addLast(occupied);
+                if(!minecart.getPassengers().isEmpty()  && !this.station) minecart.getPassengers().getFirst().sendMessage(
+                        Component.text("您已").append(
+                                        Component.text("红灯").color(TextColor.color(255,0,0)).decorate(TextDecoration.BOLD))
+                                .append(Component.text("进入"))
+                                .append(Component.text(this.dummy).color(
+                                        TextColor.fromCSSHexString(lineMap.get(this.lid).color)))
+                                .append(Component.text("，限速运行。")).appendNewline()
+                                .append(Component.text("前车通过后限速将解除。")));
+                queueing.addLast(minecart);
                 return false;
             }
         }
@@ -105,6 +154,7 @@ public class SegmentMap {
                         cart = queueing.removeFirst();
                     } while (cart.isDead());
                     occupied = cart;
+                    if(!minecart.getPassengers().isEmpty()  && !this.station) cart.getPassengers().getFirst().sendMessage("限速已解除。");
                 } else occupied = null;
             }
         }
