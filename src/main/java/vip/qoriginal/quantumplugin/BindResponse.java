db37f9896b86ba0040cf5e31deab6f8e539ec6e6
glowingstone124 <hanserofficial@outlook.com>
2024-11-15 17:52:52 +0800

    fix

--- src/main/java/vip/qoriginal/quantumplugin/BindResponse.java
+++ src/main/java/vip/qoriginal/quantumplugin/BindResponse.java
@@ -2,6 +2,7 @@ package vip.qoriginal.quantumplugin;
 
 public class BindResponse {
     public int code = -1;
+    public boolean temp = false;
     public long qq = -1;
     public boolean frozen = false;
     public boolean online = false;
