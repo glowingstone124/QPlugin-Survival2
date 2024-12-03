8a62cd7ac7fee57189068263599b13dd1564f0ab
glowingstone124 <hanserofficial@outlook.com>
2024-12-03 12:25:29 +0800

    Fix

--- src/main/java/vip/qoriginal/quantumplugin/IPUtils.java
+++ src/main/java/vip/qoriginal/quantumplugin/IPUtils.java
@@ -10,24 +10,32 @@ import vip.qoriginal.quantumplugin.patch.Utils;
 
 import java.util.Objects;
 
-public class IPUtils{
-    public static void locIsCn(PlayerJoinEvent event, Plugin plugin) throws Exception {
+public class IPUtils {
+    public static void locIsCn(PlayerJoinEvent event, Plugin plugin) {
         Logger logger = new Logger();
         Player player = event.getPlayer();
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
             try {
                 ChatSync cs = new ChatSync();
                 String ip = Objects.requireNonNull(player.getAddress()).getAddress().getHostAddress();
-                JSONObject ipLocObj = new JSONObject(Request.sendGetRequest("https://ip.shakaianee.top/" + ip + "?f=json").get());
-                System.out.println(ipLocObj);
-                logger.log("Player " + player.getName() + " logging in with an ip " + ipLocObj.getString("addr") + " in region " + ipLocObj.getString("country_code"), "IPUtils");
-                if (!ipLocObj.getString("country_code").equals("CN") || !ipLocObj.getString("country_code").isEmpty()) {
-                    player.sendMessage("你正在使用一个非中国大陆ip登录。");
-                    cs.sendChatMsg("玩家" + player.getName() + "正在使用一个非中国大陆ip登录. (" + ip + ")");
+                JSONObject ipLocObj = fetchIpLocationWithRetries(ip, 3);
+                if (ipLocObj == null) {
+                    logger.log("无法获取IP地址信息", "IPUtils");
+                    Utils.INSTANCE.runTaskOnMainThread(() ->
+                    {
+                        player.kick(Component.text("无法获取IP属地信息，看上去似乎API失效了，请联系glowingstone124."));
+                    });
+                    return;
+                }
+
+                logger.log("Player " + player.getName() + " logging in with an IP " + ipLocObj.getString("addr") + " in region " + ipLocObj.getString("country_code"), "IPUtils");
+                if (!ipLocObj.getString("country_code").equals("CN")) {
+                    player.sendMessage("你正在使用一个非中国大陆IP登录。");
+                    cs.sendChatMsg("玩家 " + player.getName() + " 正在使用一个非中国大陆IP登录");
                     if (!JoinLeaveListener.ip_whitelist.contains(ip)) {
-                        player.sendMessage("你正在使用一个非中国大陆ip登录，请联系glowingstone124过白您的ip。您的ip：" + ip);
+                        player.sendMessage("你正在使用一个非中国大陆IP登录，请联系glowingstone124过白您的IP。您的IP：" + ip);
                         Utils.INSTANCE.runTaskOnMainThread(() -> {
-                            player.kick(Component.text("你正在使用一个非中国大陆ip登录，请联系glowingstone124过白您的ip。您的ip：" + ip));
+                            player.kick(Component.text("你正在使用一个非中国大陆IP登录，请联系glowingstone124过白您的IP。您的IP：" + ip));
                         });
                     }
                 }
@@ -36,4 +44,25 @@ public class IPUtils{
             }
         });
     }
+
+    private static JSONObject fetchIpLocationWithRetries(String ip, int retries) {
+        int attempts = 0;
+        while (attempts < retries) {
+            try {
+                String response = Request.sendGetRequest("https://ip.shakaianee.top/" + ip + "?f=json").get();
+                return new JSONObject(response);
+            } catch (Exception e) {
+                attempts++;
+                System.err.println("尝试获取IP地址信息失败，第 " + attempts + " 次重试...");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException interruptedException) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
 }
