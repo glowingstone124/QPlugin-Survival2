8d010b4b04d3e970f20d9822392d9138b44d5197
glowingstone124 <hanserofficial@outlook.com>
2024-12-06 16:18:59 +0800

    finally fix

--- src/main/java/vip/qoriginal/quantumplugin/Login.kt
+++ src/main/java/vip/qoriginal/quantumplugin/Login.kt
@@ -189,7 +189,7 @@ class Login : Listener {
 			System.currentTimeMillis(),
 			success,
 		)
-		Request.sendPostRequest("http://172.19.0.6:8080/qo/upload/loginattempt", gson.toJson(logClazz), java.util.Optional.ofNullable(mapOf("token" to "aad3r32in213ndvv11@")))
+		Request.sendPostRequest("http://172.19.0.6:8080/qo/upload/loginattempt?auth=aad3r32in213ndvv11@", gson.toJson(logClazz))
 	}
 }
 data class LoginLog(
