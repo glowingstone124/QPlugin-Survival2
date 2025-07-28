985743a3cc8391af934d4864e7aef742367ef830
glowingstone124 <hanserofficial@outlook.com>
2025-07-28 22:08:50 +0800

    bump paper api version to 1.21.8

--- src/main/java/vip/qoriginal/quantumplugin/ChatCommandListener.java
+++ src/main/java/vip/qoriginal/quantumplugin/ChatCommandListener.java
@@ -8,7 +8,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatCommandListener implements Listener {
     @EventHandler
